01ffd7552921480eb8fd7eecb89405260003de1d
neel <neel2292@gmail.com>
2015-10-23 15:27:40 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 
+$(".fabs").hover(function() {
+  $(".prime").toggleClass("zmdi-plus");
+  $(".prime").toggleClass("rotateInUpLeft");
+  $(".prime").toggleClass("zmdi-account");
+  $(".sec").toggleClass("fadeInUp");
+});
+
+$(".fab").hover(function() {
+  $(this).children(".sec").toggleClass("wobble");
+  $(this).children(".sec").toggleClass("fadeInUp");
+});
+
+$(".mat-react").hover(function() {
+	$(this).children(".mat").toggleClass("wobble");
+});
